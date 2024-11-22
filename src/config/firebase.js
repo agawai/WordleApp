609a14638c9d82c42df47b588413202d3a40331f
Alvin Gawai <105524820+agawai@users.noreply.github.com>
2024-11-21 19:59:58 -0500

    updated firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,20 +3,10 @@ import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { maybeCompleteAuthSession } from 'expo-web-browser';
+import { firebaseConfig } from './firebaseConfig';
 
 // Ensure the auth redirect can complete
 maybeCompleteAuthSession();
-
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyB1-ien6C4J-2Cndd_C9Xnmaj8EP9LHVy4",
-  authDomain: "wordleapp-ef35a.firebaseapp.com",
-  projectId: "wordleapp-ef35a",
-  storageBucket: "wordleapp-ef35a.firebasestorage.app",
-  messagingSenderId: "1088510527644",
-  appId: "1:1088510527644:web:469fbcfcdadeceff778559",
-  measurementId: "G-YNFQJTH1YD"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
